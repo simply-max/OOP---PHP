a59797832d9353613c2c892f506c329fc434145b
maximilian hofheinz <maximilian.hofheinz>
2024-02-06 15:00:22 +0100

    Anpasssung durch ChatGPT

--- Harmonischer Mittelwert/Messgenauigkeit.class.php
+++ Harmonischer Mittelwert/Messgenauigkeit.class.php
@@ -2,15 +2,22 @@
 require_once "Mittelwert.adapter.php";
 
 class Messgenauigkeit extends MittelwertAdapter {
-  private array $messwerte = [10,12,14,20];
+  // Messwerte als private Eigenschaft deklarieren
+  private array $messwerte = [10, 12, 14, 20];
 
   public function showQuadratischesMittel(): void {
-    $amw = 0;
-    $mwesswqs = 0;
-    foreach($this->messwerte as $messwert) {
-      $mwesswqs += pow($messwert, 2);
+    $quadratSumme = 0;
+
+    // Sicherstellen, dass das Array nicht leer ist
+    if (!empty($this->messwerte)) {
+      foreach ($this->messwerte as $messwert) {
+          $quadratSumme += $messwert ** 2;
+      }
+
+      $amw = sqrt($quadratSumme / count($this->messwerte));
+      echo "<p>Quadratisches Mittel: $amw</p>";
+    } else {
+      echo "<p>Keine Messwerte vorhanden.</p>";
     }
-    $amw = sqrt($mwesswqs/count($this->messwerte));
-    echo "<p>Quadratisches Mittel: $amw</p>";
   }
 }
